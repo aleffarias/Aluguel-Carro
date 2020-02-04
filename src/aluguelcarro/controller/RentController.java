ba93391b3b61fbb775647b06804fe49d5d3bdce4
aleffarias <aaof@ic.ufal.br>
2020-02-04 01:35:54 -0200

    Add screens

---
+++ src/aluguelcarro/controller/RentController.java
@@ -0,0 +1,14 @@
+package aluguelcarro.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class RentController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
