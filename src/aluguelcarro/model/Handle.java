254b08a74758e39be4735fcc5f67c26af61fe9d0
aleffarias <aaof@ic.ufal.br>
2020-02-07 12:54:19 -0300

    Create class interface Handle

---
+++ src/aluguelcarro/model/Handle.java
@@ -0,0 +1,10 @@
+package aluguelcarro.model;
+
+import javafx.event.ActionEvent;
+
+public interface Handle {
+    
+    public void handleButtonClicks(ActionEvent mouseEvent);
+    public void exitAction(ActionEvent mouseEvent);
+    
+}
